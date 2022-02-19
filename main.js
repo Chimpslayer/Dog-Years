ab467aad82794ba47644c6653b8dd2233467d3ad
Chimpslayer <94501127+Chimpslayer@users.noreply.github.com>
2022-02-18 21:13:58 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+// My age
+const myAge = 8
+
+// 
+var earlyYears = 2
+earlyYears *= 10.5
+
+// My age -2 *4
+var laterYears = (myAge - 2) * 4
+
+// My age in dog years
+var myAgeInDogYears = earlyYears + laterYears
+
+// My name in lower case
+var myName = 'Isaiah'.toLowerCase()
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
